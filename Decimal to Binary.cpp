e6d0081fc3be05cab223aa202ca282e7a9994158
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-01-13 22:53:31 +0530

    Create Decimal to Binary.cpp

---
+++ Decimal to Binary.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+	cin >> n;
+	
+	long binary = 0, pow = 1;
+	while(n > 0) {
+		int lastBit = n % 2;
+		binary += lastBit * pow;
+		pow *= 10;
+		n = n/2;
+	}
+	cout << binary;
+}

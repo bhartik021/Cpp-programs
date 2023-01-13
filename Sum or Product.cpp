cd8334cb81ecddaf1e24e44d101a3c8ee215d471
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-01-13 21:33:16 +0530

    Create Sum or Product.cpp

---
+++ Sum or Product.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+	cin >> n;
+	int c;
+	cin >> c;
+	 if(c==1)
+        {
+            int sum=0;
+            for(int i=1;i<=n;i++)
+            {
+                sum+=i;
+            }
+           cout << sum << endl;
+        }
+		else if(c==2)
+        {
+            int product=1;
+            for(int i=1;i<=n;i++)
+            {
+                product*=i;
+            }
+            cout << product << endl;
+		}
+            else
+            cout << "-1" << endl;
+	}
+
